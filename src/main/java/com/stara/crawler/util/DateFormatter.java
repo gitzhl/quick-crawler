a50b7d1c0a81c8737f0be61e9374d7691305e076
gitzhl <zhl51job@sina.cn>
2015-06-19 10:38:25 +0800

    add subdir and add log

---
+++ src/main/java/com/stara/crawler/util/DateFormatter.java
@@ -0,0 +1,12 @@
+package com.stara.crawler.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateFormatter {
+	private static SimpleDateFormat sf = null;
+	public static String format(Date date,String pattern){
+		sf = new SimpleDateFormat(pattern);
+		return sf.format(date);
+	}
+}
