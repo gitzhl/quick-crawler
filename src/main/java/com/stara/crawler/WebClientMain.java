9a67b7e77254d1beee550a7311fed55fcf3536dc
gitzhl <zhl51job@sina.cn>
2015-09-02 16:15:31 +0800

    add htmlUnit libs

---
+++ src/main/java/com/stara/crawler/WebClientMain.java
@@ -0,0 +1,55 @@
+package com.stara.crawler;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Map;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+public class WebClientMain {
+    
+    private static final WebClient WEB_CLIENT = new WebClient();
+    
+    static{
+        WEB_CLIENT.getOptions().setCssEnabled(false);
+        WEB_CLIENT.getOptions().setJavaScriptEnabled(false);
+    }
+    
+    public static String asText(Map<String, String> params,String url){
+        HtmlPage page = null;
+        try {
+            page = WEB_CLIENT.getPage(url);
+            closeWebClient(WEB_CLIENT);
+            return page.asText();
+        } catch (FailingHttpStatusCodeException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public static String asXML(Map<String, String> params,String url){
+        HtmlPage page = null;
+        try {
+            page = WEB_CLIENT.getPage(url);
+            closeWebClient(WEB_CLIENT);
+            return page.asXml();
+        } catch (FailingHttpStatusCodeException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public static void closeWebClient(WebClient webClient){
+        webClient.close();
+    }
+}
