f48d4aa4206d1172e94aba94c7649eabe0d7cff3
zhuheliang <zhl51job@sina.cn>
2016-05-03 16:52:39 +0800

    add Enum content

--- src/main/java/com/stara/crawler/common/ResourceStoragePolicy.java
+++ src/main/java/com/stara/crawler/common/ResourceStoragePolicy.java
@@ -1,9 +1,17 @@
 package com.stara.crawler.common;
+
+import java.util.Date;
+
+import com.stara.crawler.util.DateFormatter;
+
 /**
  * @see 下载的资源文件命名策略
  * @author Administrator
  *
  */
 public enum ResourceStoragePolicy {
-	TIMESTAMP,UUID,NATIVE;//时间戳,UUID,原生
+	TIMESTAMP{public String geneTag(String suffix){return String.valueOf(System.currentTimeMillis()).concat(suffix);}},
+	UUID{public String geneTag(String suffix){return java.util.UUID.randomUUID().toString().concat(suffix);}},
+	PATTERDATE{public String geneTag(String suffix){return DateFormatter.format(new Date(), "yyyyMMddHHmmss").concat(suffix);}};//时间戳,UUID,原生
+	public abstract String geneTag(String suffix);
 }
